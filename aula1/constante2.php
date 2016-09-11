17fe94b51383ed7549f1e09f4ceb9dd2752199a5
Lucas Hernandes <lukasskate3@gmail.com>
2016-09-11 14:57:52 -0300

    contante
    constante versão 2

---
+++ aula1/constante2.php
@@ -0,0 +1,20 @@
+<?php
+
+define('JS', 'js/');
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="utf-8">
+     <title>Curso php</title>
+     <script src="<?php echo CSS."style.css" ?>"charset="vtf-8></script>
+  </head>
+  <body>
+      <p>
+        testando
+      </p>
+    <script src="<?= echo JS."js.js" ?>"type="text/javacript"></script>
+<body>
+</html>
