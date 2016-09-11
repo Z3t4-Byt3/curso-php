a7cde3c3bc50507bec397a69d6e893bd545399f7
Lucas Hernandes <lukasskate3@gmail.com>
2016-09-11 14:54:27 -0300

    Update iniciando.php
    atualizando a aula 1

--- aula1/iniciando.php
+++ aula1/iniciando.php
@@ -1,15 +1,15 @@
 <?php
-  $var = "Mateus";
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Curso PHP</title>
-  </head>
-  <body>
-      <p>
-        Olá, <?= $var ?>
-      </p>
+$vart = "Lucas"
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Curso PHP</title>
+ </head>
+ <body>
+     <p>
+       Olá, <?= $vart ?>
+     </p>
   </body>
 </html>
